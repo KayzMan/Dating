365ba699ee0b09d513c00542151b20f6b0d19636
Ngoni Kayzman <ngonik8@gmail.com>
2023-09-17 18:31:43 +0200

    updated Dating App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, Text } from "react-native";
+
+// 👇 screens
+// import Navigator from "./navigation/Navigator";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="default" />
+      <Text>Dating App</Text>
+      {/* <Navigator /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ navigation/Navigator.tsx
@@ -0,0 +1,17 @@
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// const Stack = createNativeStackNavigator();
+
+// // 👇 screens
+// import Home from "../screens/Home";
+
+// export default function Navigator() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="homeScreen" component={Home} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
